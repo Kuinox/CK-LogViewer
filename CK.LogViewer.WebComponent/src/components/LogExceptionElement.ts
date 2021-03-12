191aee306d06c4009022943a46bcb1b3a1253230
Dan Chiche <dan.chiche@signature-code.com>
2021-03-12 19:10:56 +0100

    Splitting code into a new function

--- CK.LogViewer.WebComponent/src/components/LogExceptionElement.ts
+++ CK.LogViewer.WebComponent/src/components/LogExceptionElement.ts
@@ -5,58 +5,14 @@ export class LogExceptionElement extends HTMLElement {
         super();
     }
 
-    openModal = () => {
-       console.log("ddd")
-    }
-
     static create(log: CKExceptionData) {
         const entry = new LogExceptionElement();
         const a = document.createElement("a");
         a.innerHTML = log.typeException;
-        a.href ="";
-        a.addEventListener('click',(e)=>{
+        a.href = "";
+        a.addEventListener('click', (e) => {
             e.preventDefault();
-            // const modal = document.createElement('dialog');
-            // const closeTag = document.createElement("span");
-            // closeTag.setAttribute("class","close");
-            // closeTag.innerHTML = "&times;";
-            // modal.appendChild(closeTag);
-            // modal.innerHTML = log.stackTrace;
-            // entry.appendChild(modal);
-            // modal.showModal();
-
-
-
-            const modal = document.createElement('div');
-            modal.setAttribute("class","modal");
-
-            document.addEventListener("click",(event)=>{
-                const target = event.target as any;
-                if(target.className === "modal"){
-                    modal.remove();
-                }
-            })
-
-            const modalContent = document.createElement('div');
-            modalContent.setAttribute("class","modal-content");
-
-            const closeTag = document.createElement("span");
-            closeTag.setAttribute("class","close");
-            closeTag.innerHTML = "&times;";
-            closeTag.addEventListener("click",()=>{
-                modal.remove();
-            })
-
-            const content = document.createElement("p");
-            content.innerHTML = log.stackTrace;
-
-            modalContent.appendChild(closeTag);
-            modalContent.appendChild(document.createElement("br"));
-            modalContent.appendChild(content);
-            modal.appendChild(modalContent)
-
-            entry.appendChild(modal);
-            // modal.showModal();
+            this.createModal(log, entry);
 
         })
         const br = document.createElement("br");
@@ -64,6 +20,38 @@ export class LogExceptionElement extends HTMLElement {
         entry.appendChild(br);
         return entry;
     }
+
+    static createModal(log: CKExceptionData, entry: LogExceptionElement) {
+        const modal = document.createElement('div');
+        modal.setAttribute("class", "modal");
+        document.addEventListener("click", (event) => {
+            const target = event.target as any;
+            if (target.className === "modal") {
+                modal.remove();
+            }
+        })
+
+        const modalContent = document.createElement('div');
+        modalContent.setAttribute("class", "modal-content");
+
+        const closeTag = document.createElement("span");
+        closeTag.setAttribute("class", "close");
+        closeTag.innerHTML = "&times;";
+        closeTag.addEventListener("click", () => {
+            modal.remove();
+        })
+
+        const content = document.createElement("p");
+        content.innerHTML = log.stackTrace;
+
+        modalContent.appendChild(closeTag);
+        modalContent.appendChild(document.createElement("br"));
+        modalContent.appendChild(content);
+        modal.appendChild(modalContent)
+
+        entry.appendChild(modal);
+    }
+
 }
 
 customElements.define('log-exception', LogExceptionElement);
