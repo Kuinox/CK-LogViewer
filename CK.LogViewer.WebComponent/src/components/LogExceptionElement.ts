c8ced90c3df2baf69f8df0247a9fd8bc15376228
Dan Chiche <dan.chiche@signature-code.com>
2021-03-12 19:05:27 +0100

    Refactoring  modal create DOM

--- CK.LogViewer.WebComponent/src/components/LogExceptionElement.ts
+++ CK.LogViewer.WebComponent/src/components/LogExceptionElement.ts
@@ -16,12 +16,52 @@ export class LogExceptionElement extends HTMLElement {
         a.href ="";
         a.addEventListener('click',(e)=>{
             e.preventDefault();
-            const modal = document.createElement('dialog');
-            modal.innerHTML = log.stackTrace;
+            // const modal = document.createElement('dialog');
+            // const closeTag = document.createElement("span");
+            // closeTag.setAttribute("class","close");
+            // closeTag.innerHTML = "&times;";
+            // modal.appendChild(closeTag);
+            // modal.innerHTML = log.stackTrace;
+            // entry.appendChild(modal);
+            // modal.showModal();
+
+
+
+            const modal = document.createElement('div');
+            modal.setAttribute("class","modal");
+
+            document.addEventListener("click",(event)=>{
+                const target = event.target as any;
+                if(target.className === "modal"){
+                    modal.remove();
+                }
+            })
+
+            const modalContent = document.createElement('div');
+            modalContent.setAttribute("class","modal-content");
+
+            const closeTag = document.createElement("span");
+            closeTag.setAttribute("class","close");
+            closeTag.innerHTML = "&times;";
+            closeTag.addEventListener("click",()=>{
+                modal.remove();
+            })
+
+            const content = document.createElement("p");
+            content.innerHTML = log.stackTrace;
+
+            modalContent.appendChild(closeTag);
+            modalContent.appendChild(document.createElement("br"));
+            modalContent.appendChild(content);
+            modal.appendChild(modalContent)
+
             entry.appendChild(modal);
-            modal.showModal();
+            // modal.showModal();
+
         })
+        const br = document.createElement("br");
         entry.appendChild(a);
+        entry.appendChild(br);
         return entry;
     }
 }
