e376f2b0992c037618468d1c59c778d9883c0bae
Kuinox <github@kuinox.io>
2021-11-14 20:40:47 +0100

    Inlined Cake addin to avoid dependencies issue.

---
+++ CodeCakeBuilder/CakeExtensions/WebDeployAliases.cs
@@ -0,0 +1,33 @@
+﻿#region Using Statements
+using Cake.Core;
+using Cake.Core.Annotations;
+
+using Microsoft.Web.Deployment;
+#endregion
+
+
+
+namespace Cake.WebDeploy
+{
+    /// <summary>
+    /// Contains Cake aliases for WebDeploy
+    /// </summary>
+    [CakeAliasCategory("WebDeploy")]
+    [CakeNamespaceImport("Microsoft.Web.Deployment")]
+    public static class WebDeployAliases
+    {
+        #region Methods
+        /// <summary>
+        /// Deploys the content of a website
+        /// </summary>
+        /// <param name="context">The cake context.</param>
+        /// <param name="settings">The deployment settings.</param>
+        /// <returns>The <see cref="DeploymentChangeSummary"/> that was applied during the deployment.</returns>
+        [CakeMethodAlias]
+        public static DeploymentChangeSummary DeployWebsite(this ICakeContext context, DeploySettings settings)
+        {
+            return new WebDeployManager(context.Environment, context.Log).Deploy(settings);
+        }
+        #endregion
+    }
+}
